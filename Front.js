057b71518490c1155d24f1cf134ed1858b7e06c3
Zero0Di <104685929+Zero0Di@users.noreply.github.com>
2024-11-14 00:35:38 -0300

    Update Front.js

--- Front.js
+++ Front.js
@@ -1,1 +1,25 @@
-
+const submeta = document.getElementById("submeta-se");
+const tampaTudo = document.getElementById("tampaTudo");
+const canva = document.getElementById("plataforma").getContext("2d");
+console.log(canva)
+const player = {
+  nome: String,
+  position: [],
+  rotation: Number
+}
+//
+submeta.addEventListener("click", (event) => {
+  if(!tampaTudo.children[0].children[0].value) {
+    alert("Precisa fornecer um nome");
+    return;
+  }
+  player.nome = tampaTudo.children[0].children[0].value;
+  tampaTudo.parentNode.removeChild(tampaTudo);
+  console.log(player)
+  canva.fillStyle = "green";
+  canva.fillRect(2, 2, 4, 1);
+});
+//
+window.addEventListener("onmousemove", (event) => {
+  console.log(event)
+});
