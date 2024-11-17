4e0faefe4b453aa2afb476ec366c7dae920c79f5
Zero0Di <104685929+Zero0Di@users.noreply.github.com>
2024-11-17 16:17:10 -0300

    Update Front.js

--- Front.js
+++ Front.js
@@ -1,12 +1,18 @@
 const submeta = document.getElementById("submeta-se");
 const tampaTudo = document.getElementById("tampaTudo");
-const canva = document.getElementById("plataforma").getContext("2d");
-console.log(canva)
+const plataformaCobrinha = document.getElementById("plataforma").getContext("2d");
+const plataforma = document.getElementById("plataforma");
+let anima;
+console.log(plataformaCobrinha);
 const player = {
   nome: String,
-  position: [],
+  tamanho: 20,
+  position: [{
+    x: 200,
+    y: 100
+  }],
   rotation: Number
-}
+};
 //
 submeta.addEventListener("click", (event) => {
   if(!tampaTudo.children[0].children[0].value) {
@@ -15,11 +21,41 @@ submeta.addEventListener("click", (event) => {
   }
   player.nome = tampaTudo.children[0].children[0].value;
   tampaTudo.parentNode.removeChild(tampaTudo);
-  console.log(player)
-  canva.fillStyle = "green";
-  canva.fillRect(2, 2, 4, 1);
+  console.log(player, document.getElementById("plataforma").width);
+  AnimaplataformaCobrinha(100, 12);
 });
 //
-window.addEventListener("onmousemove", (event) => {
-  console.log(event)
+function AnimaplataformaCobrinha(positionX, positionY) {
+  plataformaCobrinha.clearRect(0,0,5000,5000);
+  if (positionX > 5000) {
+    plataformaCobrinha.beginPath();
+    //console.log(plataformaCobrinha);
+    plataformaCobrinha.fillStyle = "green";
+    plataformaCobrinha.fillRect(player.position[0].x, player.position[0].y, 40, 24);
+    plataformaCobrinha.closePath();
+    for (let i = 0; i < player.tamanho; i++) {
+      plataformaCobrinha.beginPath();
+      plataformaCobrinha.arc((player.position[0].x-12)-(i*6), player.position[0].y+12, 12, (Math.PI/180)*0, (Math.PI/180)*360, true);
+      console.log((player.position[0].x-40)-(i*6))
+      plataformaCobrinha.fill();
+    }
+  }
+  plataformaCobrinha.beginPath();
+  //console.log(plataformaCobrinha);
+  plataformaCobrinha.fillStyle = "green";
+  plataformaCobrinha.rotate(20 * Math.PI / 180);
+  plataformaCobrinha.fillRect(player.position[0].x, player.position[0].y, 40, 24);
+  plataformaCobrinha.closePath();
+  for (let i = 0; i < player.tamanho; i++) {
+    plataformaCobrinha.beginPath();
+    plataformaCobrinha.arc((player.position[0].x-12)-(i*6), player.position[0].y+12, 12, (Math.PI/180)*0, (Math.PI/180)*360, true);
+    console.log((player.position[0].x-40)-(i*6))
+    plataformaCobrinha.fill();
+  }
+  //positionX++; it's dont work
+}
+//
+plataforma.addEventListener("mousemove", (event) => {
+  //console.log(event.clientX, event.clientY);
+  //AnimaplataformaCobrinha(event.clientX, event.clientY);
 });
